16a2804ffa1418ff040ac302d01494645d542d10
Artyom <timofeev.a.vch@gmail.com>
2024-01-29 13:10:26 +0500

    init

---
+++ renderer/pages/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Head from 'next/head'
+
+export default function RulesPage() {
+
+  return (
+    <React.Fragment>
+      <Head>
+        <title>Rules | Expert System Shell</title>
+      </Head>
+    </React.Fragment>
+  )
+}
