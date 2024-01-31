9769699801009692e7d5d273ba6281a21f55004e
Artyom <timofeev.a.vch@gmail.com>
2024-01-31 14:36:22 +0500

    add file actions menu

--- renderer/components/navbars/main.jsx
+++ renderer/components/navbars/main.jsx
@@ -1,8 +1,10 @@
+import { MainMenu } from "../menus";
 import { MainTabs } from "../tabs";
 
 const MainNavBar = () => {
   return (
     <header className="h-[65px] flex items-center px-[15px] justify-between">
+      <MainMenu/>
       <nav>
         <MainTabs/>
       </nav>
